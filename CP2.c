e607526ed599f283b7d348ba701bb2b18b1433c1
strrev-dev <deepanmaurya699@gmail.com>
2025-04-04 03:54:42 +0530

    program second

---
+++ CP2.c
@@ -0,0 +1,16 @@
+// c programs to substrct two numbers
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b,c;
+    printf("Please enter two numbers\n");
+    scanf("%d%d",&a,&b);
+    c=a-b;
+    printf("The substraction of two numbers is %d\n",c);
+    //or we can use this also
+    //printf("The substraction of two numbers is %d\n",a-b);
+    
+
+
+    return 0;
+}
