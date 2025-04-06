6dc5a7c4422296964b5b64736031d6c742b63f64
strrev-dev <deepanmaurya699@gmail.com>
2025-04-07 03:32:53 +0530

    reloved commit 1

--- CP4.c
+++ CP4.c
@@ -1,13 +1,15 @@
-//C program to print ASCII value of a character
+// C program to print ASCII value of a character
 
 #include <stdio.h>
- int main(int argc, char const *argv[])
- {
+
+int main(int argc, char const *argv[])
+{
     char ch;
-    printf("Please Enter the charachter\n");
-    scanf("%c", &ch);
-    printf("The ASCII value of %c is %d\n", ch, ch);
+    printf("Please enter a character\n");
+    scanf(" %c", &ch);  // Added space before %c
+
+    // Print the ASCII value of the character
+    printf("The ASCII value of '%c' is %d\n", ch, ch);
 
     return 0;
- }
- 
+}
