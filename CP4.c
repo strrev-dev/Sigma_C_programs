578494597a3620a9ff9139627e1f39574611a94b
strrev-dev <deepanmaurya699@gmail.com>
2025-04-07 03:05:50 +0530

    fourth commit

---
+++ CP4.c
@@ -0,0 +1,13 @@
+//C program to print ASCII value of a character
+
+#include <stdio.h>
+ int main(int argc, char const *argv[])
+ {
+    char ch;
+    printf("Please Enter the charachter\n");
+    scanf("%c", &ch);
+    printf("The ASCII value of %c is %d\n", ch, ch);
+
+    return 0;
+ }
+ 
