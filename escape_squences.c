03a670657f1dbc594dca1385de67d271ba9c5b4f
strrev-dev <deepanmaurya699@gmail.com>
2025-04-24 22:47:42 +0530

    escape squence operators

---
+++ escape_squences.c
@@ -0,0 +1,24 @@
+// C program to demonstrate example of escape sequences
+// Most popular Escape Sequences
+
+// \\	\
+// \"	"
+// \'	'
+// \?	?
+// \a	Alert
+// \b	Back space 
+// \n	New Line
+// \t	Horizontal tab
+// \v	Vertical tab
+// \r	Carriage return
+
+#include <stdio.h>
+
+int main() {
+    printf("hellow\nworld\n");
+    printf("hello \tworld\n");    
+    printf("hello \bworld\n");    
+    printf("hello \b world\n");    
+    printf("hello \?world\n");    
+
+}
